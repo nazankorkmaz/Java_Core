5dd9011c9a8b28300f2026d86c9dc98e4dad0406
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-02-26 22:50:58 +0300

    upcasting ve downcasting ornegi eklendi.

---
+++ src/main/java/com/nazankorkmaz/upDownCasting/Main.java
@@ -0,0 +1,28 @@
+package com.nazankorkmaz.upDownCasting;
+
+class Hayvan {
+    void sesCikar() {
+        System.out.println("Hayvan ses çıkarıyor");
+    }
+}
+
+class Kedi extends Hayvan {
+    void miyavla() {
+        System.out.println("Kedi miyavlıyor");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Hayvan hayvan = new Kedi();  // Upcasting (otomatik)
+        hayvan.sesCikar();          // Hayvan metoduna erişilir
+        // hayvan.miyavla();        //  Hata! Alt sınıf metoduna erişilemez
+
+
+        Hayvan hayvan2 = new Kedi();   // Upcasting
+        Kedi kedi = (Kedi) hayvan2;   // Downcasting (manuel)
+
+        kedi.sesCikar();             // Üst sınıf metoduna erişilir
+        kedi.miyavla();              // Alt sınıf metoduna erişilir
+    }
+}
